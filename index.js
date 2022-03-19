ce7ddb6a4c66bafe2c83a7de889b6cca5404f484
r0808 <arthurdelophem3@gmail.com>
2022-03-19 17:28:11 +0100

    feat: add routes

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const routes = require('./routes/imdChatRoutes');
 
 const app = express();
+
+app.use('/', routes);
 
 app.use(express.json());
 
---
+++ routes/imdChatRoutes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const imdChatController = require("./../controllers/imdChatController");
+
+//get all messages
+router.get('/api/v1/messages', imdChatController.getAll);
+
+//get message by id
+router.get('/api/v1/messages/:id', imdChatController.getMessageById);
+
+//post message
+router.post('/api/v1/messages', imdChatController.setMessage);
+
+//update message
+router.update('/api/v1/messages/:id', imdChatController.updateMessageById);
+
+//delete message
+router.delete('/api/v1/messages/:id', imdChatController.deleteMessageById);
+
+//get message by username
+//router.get('/api/v1/messages?user=username', imdChatController);
+
+
+
+module.exports = router;
