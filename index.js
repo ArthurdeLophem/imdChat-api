34a7184170f881845aa4a215c782c52d6d282388
r0808 <arthurdelophem3@gmail.com>
2022-03-21 00:43:32 +0100

    feat: add pug link to index.js

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ mongoose.connect('mongodb://localhost:27017/imdchatapp', { useNewUrlParser: true
 const app = express();
 app.use(express.json());
 
+app.set('view engine', 'pug');
+app.set('views', './views');
+app.get('/', (req, res) => {
+    res.render(index);
+})
+
 app.use('/api/v1/messages', router)
 
 app.listen(3000, () => {
